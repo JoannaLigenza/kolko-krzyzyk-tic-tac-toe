88ab49f5ca9eddb886a87f144c7a01dc7e3df865
Joanna Ligenza <39116583+JoannaLigenza@users.noreply.github.com>
2018-05-24 12:45:23 +0200

    Second version
    Wersja poprawiona

--- script.js
+++ script.js
@@ -1,75 +1,182 @@
 
 document.addEventListener('DOMContentLoaded', function() {
 	
-	// Funkcja gry - naprzemiennie wstawiamy 0 i X
 	const all_table = document.querySelector("table");
+	const all_td = document.getElementsByTagName("td");
+	const X = document.getElementById("X");
+	const zero = document.getElementById("zero");
+	const one_player = document.getElementById("one_player");
+	const two_players = document.getElementById("two_players");
+	const reset_game = document.getElementById("reset");
 	let helper = "0";
+	let player = "";
+	let winner = "";
 	
+	// Wybor X lub 0
+	one_player.addEventListener("click", function() {
+		player = "computer_play";
+		console.log(player);
+		document.getElementById("play_with").innerText = "Grasz z komputerem";
+	})
+	
+	two_players.addEventListener("click", function() {
+		player = "human_play";
+		console.log(player);
+		document.getElementById("play_with").innerText = "Grasz z inną osobą";
+	})
+	
+	X.addEventListener("click", function() {
+		helper = "X";
+		if (player == "human_play") { 
+		document.getElementById("you_choose").innerText = "Wybrałeś X \n \n Przeciwnik gra O";
+		} else { 
+		document.getElementById("you_choose").innerText = "Wybrałeś X";
+		}
+		zero.style.display = "none";
+	})
+	
+	zero.addEventListener("click", function() {
+		helper = "0";
+		if (player == "human_play") { 
+		document.getElementById("you_choose").innerText = "Wybrałeś O \n \n Przeciwnik gra X";
+		} else { 
+		document.getElementById("you_choose").innerText = "Wybrałeś O";
+		}
+		X.style.display = "none";
+	})
+	
+	reset_game.addEventListener("click", function() {
+		for ( i = 0; i < all_td.length; i = i+1 ) {
+			all_td[i].innerText = "";
+			document.querySelector("#statement").innerText = "Wynik gry:";
+			helper = "0";
+			player = "";
+			X.style.display = "inline-block";
+			zero.style.display = "inline-block";
+		}
+	})
+	
+	// Funkcja gry - naprzemiennie wstawiamy 0 i X
 	all_table.addEventListener("click", function(e){
-		
-		
 		
 		let field = e.target.closest("td").innerText
 		
-		if (document.querySelector("#statement").innerText == "Wygrana!"){ // jesli jest wygrana to wyjdz i nic nie rob wiecej
-				return;
+		if (winner == true){ 
+		//if (document.querySelector("#statement").innerText.includes("Wygrana!")){ // jesli jest wygrana to wyjdz i nic nie rob wiecej
+			return;
 		}
-		if (helper === "X" && field == ""){   			// Jesli helper, (ustawiony pierwotnie na 0) rowna sie X to wykonaj:
-		e.target.closest("td").innerText = "X";			// wpisz w najbliszy td (czyli klikniety td) X
-		helper = "0";									// i ustaw helpera na 0
-		if (document.querySelector("#statement").innerText != "Wygrana!"){  // jesli nie ma wygranej to uruchom funkcje wygrana
-		wygrana("X");
-		}
-		return;											// wyjdz i nie sprawdzaj kolejnego warunku
-		} 
 		
+		if (helper === "X" && field == ""){   				// Jesli helper rowna sie X to wykonaj:
+		e.target.closest("td").innerText = "X";				// wpisz w najbliszy td (czyli klikniety td) X
+			wynik("X"); 
+			wynik("0");
+			if (player == "") { 
+				helper = "0";
+			}
+			if (player == "human_play") { 					// i jeśli player ustawiony jest na human play to
+				helper = "0";								// ustaw helpera na 0
+			}
+			if (player == "computer_play") {				// a jesli player ustawiony jest na computer play	
+				computer_move();							// to wykonaj funkcje computer_move
+			}
+				wynik("X"); 
+				wynik("0");
+		return;												// wyjdz i nie sprawdzaj kolejnego warunku
+		} 
+			
 		if (helper === "0" && field == ""){ 
 		e.target.closest("td").innerText = "0";
-		helper = "X"; 
-		if (document.querySelector("#statement").innerText != "Wygrana!"){ 
-		wygrana("0");
-		}
+			wynik("0");
+			wynik("X");
+			if (player == "") { 
+				helper = "X";
+			}
+			if (player == "human_play") { 
+				helper = "X";
+			}
+			if (player == "computer_play") {
+				computer_move();
+			}
+				wynik("0");
+				wynik("X");
 		return;
-		} 		
+		} 
 	}) 	
+	
+	// Funkcja ruch komputera      // Znajduje wszystkie puste pola sposrod wszystkich td, dodaje je do tablicy i losuje z nich losowe pole, w ktore wstawia 0 lub X
+	function computer_move() {
+		if (winner == true) { console.log("sprawdzamy"); return; }
+		//if (document.querySelector("#statement").innerText.includes("Wygrana!")) { console.log("sprawdzamy"); return; }		// jesli jest wygrana to wyjdz i nie wykonuj kodu
+		if (document.querySelector("#statement").innerText.includes("Remis!")) { return;}
+		let new_td = "";
+		let new_td_tab = [];
+		for (i=0; i < all_td.length; i++){
+			if (all_td[i].innerText == "") {
+				new_td = all_td[i];
+				new_td_tab.push(new_td);
+			}
+		}
+		//console.log(new_td_tab.length);
+		let random_field = Math.floor((Math.random() * new_td_tab.length) + 1);
+		//console.log(random_field);
+		
+		if (helper === "X") {
+		new_td_tab[random_field-1].innerText = "0";
+		}
+		if (helper === "0") {
+		new_td_tab[random_field-1].innerText = "X";
+		}
+	}
+	
 	
 	// Funkcja sprawdzajaca wynik
 	
-	function wynik()
+	function wygrana(C)
 	{
-		document.querySelector("#statement").innerText = "Wygrana!";
+		document.querySelector("#statement").innerText = "Wygrana!" + " \n Wygrał: " + C;
+		winner = true;
 		console.log("yupi!");
 	}
 	
-	function wygrana(C)	{ 
+	function tie(){
+		document.querySelector("#statement").innerText = "Remis!";
+	}
 	
-	let new_tab = document.querySelectorAll("td");
-	//console.log(new_tab[2].innerText);
+	function wynik(C)	{ 
+	
+	let all_td_tab = [];
+	
 	
 	// sprawdzanie wyniku w poziomie (skacze co 3 komorki, bo musi byc sprawdzony 3 razy)
-	for ( i = 0; i < new_tab.length; i = i+3 )				
-	{ 	//console.log(i + new_tab[i].innerText)
-		//console.log(i+1 + new_tab[i+1].innerText)
-		//console.log(i+2 + new_tab[i+2].innerText)
-
-
-		if (new_tab[i].innerText == C && new_tab[i+1].innerText == C && new_tab[i+2].innerText == C) { 
-		wynik();
+	for ( i = 0; i < all_td.length; i = i+3 )				
+	{ 
+		if (all_td[i].innerText == C && all_td[i+1].innerText == C && all_td[i+2].innerText == C) { 
+		wygrana(C);
 		}
 	}
 	// sprawdzanie wyniku w pionie (musi byc sprawdzony 3 razy )
 	for ( i = 0; i < 3; i= i+1 )							
 	{ 	
-		if (new_tab[i].innerText == C && new_tab[i+3].innerText == C && new_tab[i+6].innerText == C) { 
-		wynik();
+		if (all_td[i].innerText == C && all_td[i+3].innerText == C && all_td[i+6].innerText == C) { 
+		wygrana(C);
 		}
 	}
-		// sprawdzanie skosow
-		if (new_tab[0].innerText == C && new_tab[4].innerText == C && new_tab[8].innerText == C) { 
-		wynik();
+	// sprawdzanie skosow
+		if (all_td[0].innerText == C && all_td[4].innerText == C && all_td[8].innerText == C) { 
+		wygrana(C);
 		}
-		if (new_tab[2].innerText == C && new_tab[4].innerText == C && new_tab[6].innerText == C) { 
-		wynik();
+		if (all_td[2].innerText == C && all_td[4].innerText == C && all_td[6].innerText == C) { 
+		wygrana(C);
+		}
+	// remis
+	for ( i = 0; i < all_td.length; i = i+1 ) {
+		if (all_td[i].innerText != "" ) {
+			all_td_tab.push(all_td[i]);
+		}}
+	//console.log(all_td_tab.length);
+		if (all_td_tab.length == 9 && winner != true) {
+		//if (all_td_tab.length == 9 && document.querySelector("#statement").innerText !== "Wygrana!") {
+			tie();
 		}
 	}	
 });
